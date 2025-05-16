f8f38f999cc74dc3498aa4dc5ccc2f57f6cc0ec8
umarhussa <sadiarizwan1985@gmail.com>
2025-05-16 11:37:09 +0500

    Configure Next.js to use Babel instead of SWC

--- next.config.js
+++ next.config.js
@@ -1,23 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
+  swcMinify: false,
+  experimental: {
+    swcMinify: false,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    domains: ["localhost", "placekitten.com", "picsum.photos"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-    unoptimized: true,
-  },
-  output: "standalone",
 }
 
 module.exports = nextConfig
